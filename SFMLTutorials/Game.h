0bfaeaa53cef573e6fd232861fabc1fbb07e138c
justsicklife <justsicklife@gmail.com>
2025-04-05 18:59:07 +0900

    초기 커밋.

---
+++ SFMLTutorials/Game.h
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+
+#pragma once
+
+void Begin(sf::RenderWindow& window);
+
+void Update(float deltaTime);
+
+void Render(sf::RenderWindow& window);
---
+++ SFMLTutorials/Main.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+int main() {
+	// window 객체를 생성하는 생성자인데 
+	sf::RenderWindow window(sf::VideoMode(sf::Vector2u(1200,900)), "BATTER Mario");
+
+	// deltaClock 이라는 객체를 생성
+	sf::Clock deltaClock;
+
+	Begin(window);
+
+	// 화면이 열려있으면
+	while (window.isOpen())
+	{	
+		// 프레임과 프레임의 간격 시간
+		float deltaTime = deltaClock.restart().asSeconds();
+
+		// pool 이벤트가 반환됬을떄 실행
+		while (const std::optional event = window.pollEvent())
+		{
+			// 창 닫기 코드면 
+			if (event->is<sf::Event::Closed>())
+			{
+				// 창닫음
+				window.close();
+			}
+		}
+
+		Update(deltaTime);
+
+		// 이전 프레임 지우기
+		window.clear();
+		
+		// 화면에 그릴 요소 정하기
+		Render(window);
+
+		// 내부 버퍼를 그리고 이걸하면 버퍼를 화면에 전환한다
+		window.display();
+	}
+}
